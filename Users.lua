74ca4126a0285fae34630d22167650d7a53e978f
scriptclient <rocksd333@gmail.com>
2025-08-19 18:26:48 -0300

    Adicionado Go_p100 ao painel

--- Users.lua
+++ Users.lua
@@ -2,6 +2,7 @@ allowedUsers = {
    ["tui1392"] = true, --1 semana
    ["contasegudariadesv"] = true, --✅️Permanente
    ["renanqkkd35"] = true, --até 21 de agosto
+   ["Go_p100"] = true, --até 20 de agosto
 }
 
 return allowedUsers
