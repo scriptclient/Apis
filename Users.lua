dde8d3ade87d16e1280d21ad83fa3073a7e95cba
scriptclient <rocksd333@gmail.com>
2025-08-19 18:19:40 -0300

    Adicionado panderercc ao painel

--- Users.lua
+++ Users.lua
@@ -2,6 +2,7 @@ allowedUsers = {
    ["Cort_perm"] = true, --22 de agosto
    ["Soy_morenin1329"] = true, --2 de setembro
    ["llenado123456"] = true, --permanente
+   ["panderercc"] = true, --1 semana
 }
 
 return allowedUsers
