634a1a96ca46c93d0fbe9dc34e4eafd484688f1d
scriptclient <rocksd333@gmail.com>
2025-08-19 18:19:57 -0300

    Adicionado joao96874 ao painel

--- Users.lua
+++ Users.lua
@@ -4,6 +4,7 @@ allowedUsers = {
    ["llenado123456"] = true, --permanente
    ["panderercc"] = true, --1 semana
    ["smulem_01"] = true, --permanente
+   ["joao96874"] = true, --1 semana
 }
 
 return allowedUsers
