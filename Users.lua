1a477fe2b824a227847d847c068c70b145af84f1
scriptclient <rocksd333@gmail.com>
2025-09-24 20:34:24 -0300

    Removido teste do painel

--- Users.lua
+++ Users.lua
@@ -2,7 +2,6 @@ allowedUsers = {
    ["GabrielDaTi203"] = true, --até 20/3
    ["gagadabri82"] = true, --até 30/10/2099
    ["gagagagaga"] = true, --até 10/10/2035
-   ["teste"] = true, --até 10/10/2025
 }
 
 return allowedUsers
