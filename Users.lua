8772875562d3b96109c8695791542093d3e80f04
scriptclient <rocksd333@gmail.com>
2025-09-24 20:34:10 -0300

    Adicionado teste ao painel

--- Users.lua
+++ Users.lua
@@ -2,6 +2,7 @@ allowedUsers = {
    ["GabrielDaTi203"] = true, --até 20/3
    ["gagadabri82"] = true, --até 30/10/2099
    ["gagagagaga"] = true, --até 10/10/2035
+   ["teste"] = true, --até 10/10/2025
 }
 
 return allowedUsers
