8b91506be46fd0b038d748f26e6b4f6d6c6f4b1c
scriptclient <rocksd333@gmail.com>
2025-09-25 18:45:27 -0300

    🔑 Key Manager: games (válida 10/09)

--- Users.lua
+++ Users.lua
@@ -1,6 +1,7 @@
 allowedUsers = {
     ["scout7ixs"] = true,   --Tempo: 17/09 10/09,
-    ["kronoa"] = true,   --Tempo: 10/10
+    ["kronoa"] = true,   --Tempo: 10/10,
+    ["games"] = true,   --Tempo: 10/09
  
 
 }
