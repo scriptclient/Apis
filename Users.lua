aa5c2b346b87eacc876cedd949e344b49cff80ec
scriptclient <rocksd333@gmail.com>
2025-09-25 18:42:01 -0300

    Update Users.lua

--- Users.lua
+++ Users.lua
@@ -1,7 +1,4 @@
-allowedUsers = {    ["teste"] = true,   --Tempo: sempre    ["scout7ixs"] = true,   --Tempo: 19/09 25/09
-    ["tes"] = true,   --Tempo: oioi,
-    ["gera"] = true,   --Tempo: hoje,
-    ["aiai"] = true,   --Tempo: ate
+allowedUsers = { 
 
 }
 
