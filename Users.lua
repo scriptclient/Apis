b04f5f6578958467cab2d16c8cc28faf10e36a4e
scriptclient <rocksd333@gmail.com>
2025-09-25 19:25:55 -0300

    🔑 Key Manager: levono (válida 90)

--- Users.lua
+++ Users.lua
@@ -3,7 +3,8 @@ allowedUsers = {
     ["kronoa"] = true,   --Tempo: 10/10,
     ["games"] = true,   --Tempo: 10/09,
     ["infor"] = true,   --Tempo: 09/10 ✓,
-    ["ronoazoro"] = true,   --Tempo: 09/10 09/19
+    ["ronoazoro"] = true,   --Tempo: 09/10 09/19,
+    ["levono"] = true,   --Tempo: 90
     
 }
 
