daf60b943058b84679506a9f49c297360687882c
scriptclient <rocksd333@gmail.com>
2025-09-25 19:56:20 -0300

    Key Removed: scout7ixs

--- Users.lua
+++ Users.lua
@@ -1,5 +1,4 @@
 allowedUsers = {
-    ["scout7ixs"] = true,   --Tempo: 17/09 10/09,
     ["kronoa"] = true,   --Tempo: 10/10,
     ["games"] = true,   --Tempo: 10/09,
     ["infor"] = true,   --Tempo: 09/10 ✓,
