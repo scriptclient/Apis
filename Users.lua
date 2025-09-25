5e98ccc5d46bb1d73824619758e45a0c85a875b4
scriptclient <rocksd333@gmail.com>
2025-09-25 09:54:43 -0300

    Removido a do painel

--- Users.lua
+++ Users.lua
@@ -1,7 +1,6 @@
 allowedUsers = {
    ["scout7ix"] = true, --indefinido
    ["teste"] = true, --até 1 dia
-   ["a"] = true, --indefinido
    ["s"] = true, --indefinido
    ["b"] = true, --indefinido
 }
