39a1d35aebad1664bfa213277bf3fb1eefbe1074
scriptclient <rocksd333@gmail.com>
2025-09-25 22:35:31 -0300

    Key Removed: kronoa

--- Users.lua
+++ Users.lua
@@ -1,5 +1,4 @@
 allowedUsers = {
-    ["kronoa"] = true,   --Tempo: 10/10,
     ["games"] = true,   --Tempo: 10/09,
     ["infor"] = true,   --Tempo: 09/10 ✓,
     ["ronoazoro"] = true,   --Tempo: 09/10 09/19,
