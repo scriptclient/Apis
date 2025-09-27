e1c8e10c6101259bc6dc581d526ccbdda3f1e098
scriptclient <rocksd333@gmail.com>
2025-09-27 12:29:54 -0300

    Update Users.lua

--- Users.lua
+++ Users.lua
@@ -1,9 +1,5 @@
 allowedUsers = {
-    ["scout7ixs"] = true,   --Tempo: permanente,
-    ["SCOUT8IXS"] = true,   --Tempo: 09/ 09/
 
-    
 }
 
 return allowedUsers
- -- ESSE AQUI DA ACESSO AO PAINEL PELO BOT
