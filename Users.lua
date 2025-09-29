e14eb93fdc9a8d2196076d85b2416d9ba226c20c
scriptclient <rocksd333@gmail.com>
2025-09-28 22:39:27 -0300

    Key Manager: RonnieOsborne341 (válida 9/11)

--- Users.lua
+++ Users.lua
@@ -2,7 +2,8 @@
 allowedUsers = {
     ["testename00"] = true,   --Tempo: teste,
     ["ty4buys58932"] = true,   --Tempo: staff,
-    ["Kamado_ramiro"] = true,   --Tempo: 31/09
+    ["Kamado_ramiro"] = true,   --Tempo: 31/09,
+    ["RonnieOsborne341"] = true,   --Tempo: 9/11
 
 }
 
