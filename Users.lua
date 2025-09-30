8a1e1b0ac9bed08f8ae888c669e5320c1cff6b2e
scriptclient <rocksd333@gmail.com>
2025-09-30 09:09:24 -0300

    Key Removed: testename00

--- Users.lua
+++ Users.lua
@@ -1,6 +1,5 @@
 --------/// bot add ///---------
 allowedUsers = {
-    ["testename00"] = true,   --Tempo: teste,
     ["ty4buys58932"] = true,   --Tempo: staff,
     ["Kamado_ramiro"] = true,   --Tempo: 31/09,
     ["RonnieOsborne341"] = true,   --Tempo: 9/11,
