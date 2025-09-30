ab575983adde0f3f0c63269714ad5cbece5a0489
scriptclient <rocksd333@gmail.com>
2025-09-30 13:12:32 -0300

    Key Manager: naosou_shscript (válida 30/11)

--- Users.lua
+++ Users.lua
@@ -4,7 +4,8 @@ allowedUsers = {
     ["Kamado_ramiro"] = true,   --Tempo: 31/09,
     ["RonnieOsborne341"] = true,   --Tempo: 9/11,
     ["scout7ixs"] = true,   --Tempo: 90,
-    ["tsunami24866"] = true,   --Tempo: 02/10
+    ["tsunami24866"] = true,   --Tempo: 02/10,
+    ["naosou_shscript"] = true,   --Tempo: 30/11
 
 }
 
