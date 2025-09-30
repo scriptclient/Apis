548aee2e071bdf2f8e0edbc1a7938b94db084993
scriptclient <rocksd333@gmail.com>
2025-09-30 11:58:23 -0300

    Key Manager: tsunami24866 (válida 02/10)

--- Users.lua
+++ Users.lua
@@ -3,7 +3,8 @@ allowedUsers = {
     ["ty4buys58932"] = true,   --Tempo: staff,
     ["Kamado_ramiro"] = true,   --Tempo: 31/09,
     ["RonnieOsborne341"] = true,   --Tempo: 9/11,
-    ["scout7ixs"] = true,   --Tempo: 90
+    ["scout7ixs"] = true,   --Tempo: 90,
+    ["tsunami24866"] = true,   --Tempo: 02/10
 
 }
 
