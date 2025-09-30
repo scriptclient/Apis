3e8bc85837e8d387c49683e92edde4966748bbf2
scriptclient <rocksd333@gmail.com>
2025-09-30 09:12:23 -0300

    Key Manager: scout7ixs (válida 06/10)

--- Users.lua
+++ Users.lua
@@ -3,6 +3,7 @@ allowedUsers = {
     ["ty4buys58932"] = true,   --Tempo: staff,
     ["Kamado_ramiro"] = true,   --Tempo: 31/09,
     ["RonnieOsborne341"] = true,   --Tempo: 9/11,
+    ["scout7ixs"] = true,   --Tempo: 06/10
 
 }
 
