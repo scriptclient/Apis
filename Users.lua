93d47b3ef890603a9c60f085355a14900e1704fa
scriptclient <rocksd333@gmail.com>
2025-09-30 14:34:22 -0300

    Key Manager: John_blox728 (válida teste)

--- Users.lua
+++ Users.lua
@@ -6,6 +6,7 @@ allowedUsers = {
     ["scout7ixs"] = true,   --Tempo: 90,
     ["tsunami24866"] = true,   --Tempo: 02/10,
     ["naosou_shscript"] = true,   --Tempo: 30/11,
+    ["John_blox728"] = true,   --Tempo: teste
 
 }
 
