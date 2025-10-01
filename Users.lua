e3f8b91ed1656713b337e41a32b283cc3fc495f2
scriptclient <rocksd333@gmail.com>
2025-10-01 08:56:12 -0300

    Key Manager: rafa123wx8 (válida 4/10)

--- Users.lua
+++ Users.lua
@@ -6,7 +6,8 @@ allowedUsers = {
     ["tsunami24866"] = true,   --Tempo: 02/10,
     ["naosou_shscript"] = true,   --Tempo: 30/11,
     ["ttyyryjuh"] = true,   --Tempo: 06/10,
-    ["serveston"] = true,   --Tempo: 30/10
+    ["serveston"] = true,   --Tempo: 30/10,
+    ["rafa123wx8"] = true,   --Tempo: 4/10
 
 }
 
