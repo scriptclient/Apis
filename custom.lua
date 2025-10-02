e8c6a0528864260f8fc0caed556f2a4fe13f9249
scriptclient <rocksd333@gmail.com>
2025-10-02 15:41:35 -0300

    Nick Personalizado: Kauwh14 -> "05/10"

--- custom.lua
+++ custom.lua
@@ -3,7 +3,6 @@ return {
     ["testename00"] = "MAGNUS",
     ["Roblox_Anthex"] = "O melhor",
     ["Robsn1997"] = "Magnésio",
-    ["Stealabrainort_13"] = "muito burro",
-    ["Kauwh14"] = "05/10",
+    ["Stealabrainort_13"] = "muito burro",    ["Kauwh14"] = "05/10",
 
 }
