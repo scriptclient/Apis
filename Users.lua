7738e9baaef535f513a05b8299dc44d8637b86ec
scriptclient <rocksd333@gmail.com>
2025-10-02 10:00:09 -0300

    Key Manager: Kiko_reizinho123 (válida 02/10 02/11)

--- Users.lua
+++ Users.lua
@@ -12,7 +12,8 @@ allowedUsers = {
     ["coquetteadmintest"] = true,   --Tempo: até 04/10,
     ["DAVI1235825"] = true,   --Tempo: 4/10,
     ["ywb1n2m1mjqj"] = true,   --Tempo: até 04/10,
-    ["seupaiemeu778"] = true,   --Tempo: 09/10
+    ["seupaiemeu778"] = true,   --Tempo: 09/10,
+    ["Kiko_reizinho123"] = true,   --Tempo: 02/10 02/11
 
 }
 
