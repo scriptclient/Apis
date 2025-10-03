2477ebd6a8fea77af0bd50b95906ab359d019179
scriptclient <rocksd333@gmail.com>
2025-10-03 00:01:31 -0300

    Key Removed: itiz_rei0

--- Users.lua
+++ Users.lua
@@ -7,7 +7,6 @@ allowedUsers = {
     ["ttyyryjuh"] = true,   --Tempo: 06/10,
     ["serveston"] = true,   --Tempo: 30/10,
     ["rafa123wx8"] = true,   --Tempo: 4/10,
-    ["itiz_rei0"] = true,   --Tempo: 03/10,
     ["coquetteadmintest"] = true,   --Tempo: até 04/10,
     ["DAVI1235825"] = true,   --Tempo: 4/10,
     ["ywb1n2m1mjqj"] = true,   --Tempo: até 04/10,
