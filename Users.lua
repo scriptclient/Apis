143561617188ee7f8edca3bd3fad78b50ffd0869
scriptclient <rocksd333@gmail.com>
2025-10-03 19:41:37 -0300

    Key Manager: antoni_zznk (válida 10/10)

--- Users.lua
+++ Users.lua
@@ -17,8 +17,7 @@ allowedUsers = {
     ["pn0jOZS9"] = true,   --Tempo: 09/10,
     ["SouNubDm5"] = true,   --Tempo: 02/02,
     ["Kauwh14"] = true,   --Tempo: 05/10,
-    ["Bakugo_Master4"] = true,   --Tempo: 05/10,
-    ["antoni_zznk"] = true,   --Tempo: 05/10,
+    ["Bakugo_Master4"] = true,   --Tempo: 05/10,    ["antoni_zznk"] = true,   --Tempo: 10/10
     ["brenardao9"] = true,   --Tempo: perm,
     ["wisuhwheueyehdydyd"] = true,   --Tempo: 02/03,
     ["morangodoamor_311"] = true,   --Tempo: 07/10,
