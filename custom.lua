079a455fda17bd54b0280fb687012890fbbf1b33
scriptclient <rocksd333@gmail.com>
2025-10-02 21:07:02 -0300

    Update custom.lua

--- custom.lua
+++ custom.lua
@@ -1,8 +1,4 @@
 ------/// NICKS - PERSON ///------
 return {
-    ["testename00"] = "MAGNUS",
-    ["Roblox_Anthex"] = "O melhor",
-    ["Robsn1997"] = "Magnésio",
-    ["Stealabrainort_13"] = "muito burro",    ["Kauwh14"] = "05/10",
 
 }
