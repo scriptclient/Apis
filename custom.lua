d524ba7a94e07dec4e670192dfdf05d47f5f76b4
scriptclient <rocksd333@gmail.com>
2025-10-03 22:33:41 -0300

    Nick Personalizado: derrok0076 -> "Modder"

--- custom.lua
+++ custom.lua
@@ -1,7 +1,7 @@
 ------/// NICKS - PERSON ///------
 return {
     ["Riggs_scott"] = "Modder",
-    ["pozella999"] = "P o z z",    ["derrok0076"] = "NandoS2",
+    ["pozella999"] = "P o z z",    ["derrok0076"] = "Modder",
     ["gbugado20"] = "moderador",
     ["derrok076"] = "modder",
 
