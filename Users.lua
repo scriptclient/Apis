31641df38ac79fe2c3a90e7fb63a42e68a72c38a
scriptclient <rocksd333@gmail.com>
2025-10-04 05:14:22 -0300

    Key Manager: Saliencia4_0 (válida 07/10)

--- Users.lua
+++ Users.lua
@@ -42,6 +42,7 @@ allowedUsers = {
     ["HBT_JU7cLA0MMp"] = true,   --Tempo: 06/10,
     ["chato_1001"] = true,   --Tempo: 06/10,
     ["amhnkx"] = true,   --Tempo: 11/10,
+    ["Saliencia4_0"] = true,   --Tempo: 07/10
 
 }
 
