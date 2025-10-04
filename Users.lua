a39dd9b89c67de4b321205da2895f65f95285869
scriptclient <rocksd333@gmail.com>
2025-10-04 09:57:06 -0300

    Key Manager: Soleil_noir0 (válida 07/10)

--- Users.lua
+++ Users.lua
@@ -42,8 +42,7 @@ allowedUsers = {
     ["HBT_JU7cLA0MMp"] = true,   --Tempo: 06/10,
     ["chato_1001"] = true,   --Tempo: 06/10,
     ["amhnkx"] = true,   --Tempo: 11/10,
-    ["Saliencia4_0"] = true,   --Tempo: 07/10,
-    ["Soleil_noir0"] = true,   --Tempo: 07/10,    ["davisinhodoroblok"] = true,   --Tempo: 11/10
+    ["Saliencia4_0"] = true,   --Tempo: 07/10,    ["Soleil_noir0"] = true,   --Tempo: 07/10
 
 }
 
