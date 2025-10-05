4a9d682d728cbb57782c6dc7c6e3a96b537fe9de
scriptclient <rocksd333@gmail.com>
2025-10-04 21:28:32 -0300

    Update custom.lua

--- custom.lua
+++ custom.lua
@@ -1,9 +1,3 @@
 ------/// NICKS - PERSON ///------
 return {
-    ["Riggs_scott"] = "Modder",
-    ["pozella999"] = "P o z z",    ["derrok0076"] = "Modder",
-    ["gbugado20"] = "moderador",
-    ["derrok076"] = "modder",
-
-
 }
