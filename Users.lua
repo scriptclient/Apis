84fcd455ca6c2b00010d0c2053aaef93b76d68dc
scriptclient <rocksd333@gmail.com>
2025-10-04 21:37:19 -0300

    Key Removed: rafa123wx8

--- Users.lua
+++ Users.lua
@@ -5,7 +5,6 @@ allowedUsers = {
     ["naosou_shscript"] = true,   --Tempo: 30/11,
     ["ttyyryjuh"] = true,   --Tempo: 06/10,
     ["serveston"] = true,   --Tempo: 30/10,
-    ["rafa123wx8"] = true,   --Tempo: 4/10,
     ["coquetteadmintest"] = true,   --Tempo: até 04/10,
     ["DAVI1235825"] = true,   --Tempo: 4/10,
     ["ywb1n2m1mjqj"] = true,   --Tempo: até 04/10,
