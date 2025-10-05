90a7d34d3fcf7b2efa25199a39ea42a4d952c356
scriptclient <rocksd333@gmail.com>
2025-10-04 21:38:49 -0300

    Key Removed: DAVI1235825

--- Users.lua
+++ Users.lua
@@ -5,7 +5,6 @@ allowedUsers = {
     ["naosou_shscript"] = true,   --Tempo: 30/11,
     ["ttyyryjuh"] = true,   --Tempo: 06/10,
     ["serveston"] = true,   --Tempo: 30/10,
-    ["DAVI1235825"] = true,   --Tempo: 4/10,
     ["ywb1n2m1mjqj"] = true,   --Tempo: até 04/10,
     ["seupaiemeu778"] = true,   --Tempo: 09/10,
     ["Kiko_reizinho123"] = true,   --Tempo: 02/10 02/11,
