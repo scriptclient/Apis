c77b48ee387caae9444dce201646af1c99aed73f
scriptclient <rocksd333@gmail.com>
2025-10-05 16:14:53 -0300

    Key Manager: jhowzzzzzzzzzzz (válida 12/10)

--- Users.lua
+++ Users.lua
@@ -56,7 +56,8 @@ allowedUsers = {
     ["lucasf_342"] = true,   -- 55° lucasf_342 -- 08/10 -- Sem
     ["Novo"] = true,   -- 56° Novo -- Permanente -- <@1401226694324523113>
     ["Novo1"] = true,   -- 57° Novo1 -- Permanente -- <@1058360401210904599>,
-    ["3el887"] = true,   -- 58° 3el887 -- Permanente -- <@1415502449342677125>
+    ["3el887"] = true,   -- 58° 3el887 -- Permanente -- <@1415502449342677125>,
+    ["jhowzzzzzzzzzzz"] = true,   -- 59° jhowzzzzzzzzzzz -- 12/10 -- <@1203063938754027640>
 }
 
 return allowedUsers
