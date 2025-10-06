ee4197667e7ba25dc41c84944b6e158c69b21780
scriptclient <rocksd333@gmail.com>
2025-10-06 18:17:24 -0300

    Key Manager: Michaelnatasha018 (válida 09/10)

--- Users.lua
+++ Users.lua
@@ -71,7 +71,8 @@ allowedUsers = {
     ["yhxdhbc"] = true,   -- 71° yhxdhbc -- 09/10 -- <@1221603894405238834>,
     ["God_Shazan24"] = true,   -- 71° God_Shazan24 -- 05/12 -- <@1037707209649434644>,
     ["magzin666"] = true,   -- 72° magzin666 -- 09/10 -- <@1190150565703975012>,
-    ["matheusrbicalho"] = true,   -- 73° matheusrbicalho -- 09/10 -- <@1069591954574286848>
+    ["matheusrbicalho"] = true,   -- 73° matheusrbicalho -- 09/10 -- <@1069591954574286848>,
+    ["Michaelnatasha018"] = true,   -- 74° Michaelnatasha018 -- 09/10 -- <@1376443524337111081>
 }
 
 return allowedUsers
