d065973620bd3a24c50bc30397dd0a947bda45ee
scriptclient <rocksd333@gmail.com>
2025-10-07 20:06:32 -0300

    Update samba.lua

--- samba.lua
+++ samba.lua
@@ -1,5 +1,17 @@
 local Musics = {
-
+        { Name = "sabadão", Id = 104417510874127 },
+        { Name = "Samba", Id = 1836175030 },
+        { Name = "Samba2", Id = 1838888602 },
+        { Name = "beats", Id = 16190782181 },
+        { Name = "No Solinho", Id = 72440232513341 },
+        { Name = "Cintura de Ciclone", Id = 72440232513341 },
+        { Name = "Boa vibe em Ubatuba", Id = 139059061493558 },
+        { Name = "Garoto de Copacabana", Id = 135648634110254 },
+        { Name = "Love (espere 15 segundos)", Id = 137015078564668 },
+        { Name = "Virou", Id = 111817954627349 },
+        { Name = "lição da vida", Id = 82805460494325 },
+        { Name = "sem coração", Id = 131582258401093 },
+        { Name = "faz a rodinha", Id = 127013133418864 },
 }
 
 return Musics
