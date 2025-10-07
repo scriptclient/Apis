54eb107d97f0edd73e61a556e588dda93162c411
scriptclient <rocksd333@gmail.com>
2025-10-07 20:04:30 -0300

    Update forrozin.lua

--- forrozin.lua
+++ forrozin.lua
@@ -1,5 +1,15 @@
 local Musics = {
-
+        { Name = "forró ja cansou", Id = 74812784884330 },
+        { Name = "forró da resenha", Id = 120973520531216 },
+        { Name = "forró dudu", Id = 74404168179733 },
+        { Name = "forró sao joao", Id = 106364874935196 },
+        { Name = "forró engraçado paia", Id = 76524290482399 },
+        { Name = "forro braboo", Id = 124028850681797 },
+        { Name = "100% forro vaquejada", Id = 92295159623916 },
+        { Name = "Forro", Id = 106958630419629 },
+        { Name = "Fim de semana", Id = 106185171358009 },
+        { Name = "eu já estava bem", Id = 93590122047380 },
+        { Name = "Soldado", Id = 132311372545619 },
 }
 
 return Musics
