af8f50145c1e0713848d9c8a1296ae23f3697225
scriptclient <rocksd333@gmail.com>
2025-10-07 20:06:09 -0300

    Update Trap.lua

--- Trap.lua
+++ Trap.lua
@@ -1,5 +1,14 @@
 local Musics = {
-
+        { Name = "Trap", Id = 131582258401093 },
+        { Name = "Blassed Mane", Id = 16831108393 },
+        { Name = "UNO ZERO", Id = 112959083808887 },
+        { Name = "Iate do Neymar", Id = 135738534706063 },
+        { Name = "BATIDÃO NA ALDEIA", Id = 79953696595578 },
+        { Name = "V7", Id = 80348640826643 },
+        { Name = "PARA NÃO ESQUECER QUEM SOMOS", Id = 88937498361674 },
+        { Name = "SLIP AWAY", Id = 126152928520174 },
+        { Name = "ABYSS", Id = 87237322956694 },
+        { Name = "APATHY", Id = 95288141086290 },
 }
 
 return Musics
