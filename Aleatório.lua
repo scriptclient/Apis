b371cd7ff66f75fd7057eb0916619018916e7786
scriptclient <rocksd333@gmail.com>
2025-10-07 20:06:54 -0300

    Update Aleatório.lua

--- Aleatório.lua
+++ Aleatório.lua
@@ -1,5 +1,12 @@
 local Musics = {
-
+        { Name = "Carro de Ovos", Id = 3148329638 },
+        { Name = "Megalovania", Id = 104500091160463 },
+        { Name = "NIGHT DANCER", Id = 113900088691832 },
+        { Name = "Bubblegum", Id = 125532150673318 },
+        { Name = "Alone in Motion", Id = 122379348696948 },
+        { Name = "Lifelong Memory", Id = 81929101024622 },
+        { Name = "Game Over!", Id = 96043025049080 },
+        { Name = "Camino", Id = 82628621841537 },
 }
 
 return Musics
