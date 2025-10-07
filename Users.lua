9e17b57a08c7700a19e59678ec327ac42e79cdcc
scriptclient <rocksd333@gmail.com>
2025-10-07 01:32:33 -0300

    Key Manager: Firecrowblackice (válida 07/03)

--- Users.lua
+++ Users.lua
@@ -60,7 +60,8 @@ allowedUsers = {
     ["Japaqteeama"] = true,   -- 58° Japaqteeama -- 13/10 -- <@1397328303878312059>
     ["kqqllk_268659"] = true,   -- 59° kqqllk_268659 -- 09/10 -- <@1094384728582410240>
     ["LULA13XVDEO"] = true,   -- 60° LULA13XVDEO -- 13/10 -- <@1379140469740535942>
-    ["lufe_s"] = true,   -- 61° lufe_s -- 27/10 -- <@1354736279962652712>
+    ["lufe_s"] = true,   -- 61° lufe_s -- 27/10 -- <@1354736279962652712>,
+    ["Firecrowblackice"] = true,   -- 63° Firecrowblackice -- 07/03 -- <@1221327254425043036>
 }
 
 return allowedUsers
