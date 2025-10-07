aedd5bad36ae1de4b292f6c2cf74229be91225cd
scriptclient <rocksd333@gmail.com>
2025-10-07 00:00:56 -0300

    Update custom.lua

--- custom.lua
+++ custom.lua
@@ -1,7 +1,5 @@
 ------/// NICKS - PERSON ///------
 return {
     ["Kaua_452"] = "Supervisor",
-    ["derrok0076"] = "Modder",
-    ["vendasbraint"] = "Moderador",    ["ty4buys58932"] = "moderador",
 
 }
