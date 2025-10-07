f5ca853a876d011bc60e22de976585721a0915c5
scriptclient <rocksd333@gmail.com>
2025-10-06 23:30:26 -0300

    Key Manager: kqqllk_268659 (válida 09/10)

--- Users.lua
+++ Users.lua
@@ -72,7 +72,8 @@ allowedUsers = {
     ["Michaelnatasha018"] = true,   -- 74° Michaelnatasha018 -- 09/10 -- <@1376443524337111081>,
     ["Japaqteeama"] = true,   -- 75° Japaqteeama -- 13/10 -- <@1397328303878312059>,
     ["Davy_686"] = true,   -- 76° Davy_686 -- 09/10 -- <@1424902476212994110>,
-    ["eudhhhsllsp"] = true,   -- 77° eudhhhsllsp -- 7/10 -- <@1259160145389420565>
+    ["eudhhhsllsp"] = true,   -- 77° eudhhhsllsp -- 7/10 -- <@1259160145389420565>,
+    ["kqqllk_268659"] = true,   -- 75° kqqllk_268659 -- 09/10 -- <@1094384728582410240>
 }
 
 return allowedUsers
