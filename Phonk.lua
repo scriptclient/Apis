f2c91a73b30800ed29bdfaeee22d22dd47062e7e
scriptclient <rocksd333@gmail.com>
2025-10-07 18:41:29 -0300

    Add music: Teste to Phonk

--- Phonk.lua
+++ Phonk.lua
@@ -2,6 +2,7 @@ local Musics = {
     { Name = "Songs", Id = 208392 },
     { Name = "Vip", Id = 2037929 },
     { Name = "NewSong", Id = 297329 },
+    { Name = "Teste", Id = 356543334 },
 }
 
 return Musics
