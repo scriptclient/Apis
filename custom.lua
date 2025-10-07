523c3277c63f8ed34afa228fc1e88e673be760da
scriptclient <rocksd333@gmail.com>
2025-10-07 00:00:22 -0300

    Nick Personalizado: ty4buys58932 -> "moderador"

--- custom.lua
+++ custom.lua
@@ -2,7 +2,6 @@
 return {
     ["Kaua_452"] = "Supervisor",
     ["derrok0076"] = "Modder",
-    ["vendasbraint"] = "Moderador",
-    ["ty4buys58932"] = "modder",
+    ["vendasbraint"] = "Moderador",    ["ty4buys58932"] = "moderador",
 
 }
