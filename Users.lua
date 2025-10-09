990dde5f2e50e46f950336db8e6a6a19fe2e0fa8
scriptclient <rocksd333@gmail.com>
2025-10-09 07:53:14 -0300

    Key Manager: deixa esse acesso aqui pra não buga (válida tentando arrumar bug)

--- Users.lua
+++ Users.lua
@@ -68,6 +68,7 @@ allowedUsers = {
     ["HBT_JU7cLA0MMP"] = true,   -- 80° HBT_JU7cLA0MMP -- 09/12 -- <@1292849634414428325>,
     ["Joaodopaograo"] = true,   -- 81° Joaodopaograo -- 15/10 -- <@1125619879060447252>,
     ["acapsavass2"] = true,   -- 69° acapsavass2 -- 09/11 -- <@1166162590808936599>,
+    ["deixa esse acesso aqui pra não buga"] = true,   -- 70° deixa esse acesso aqui pra não buga -- tentando arrumar bug -- <@1058360401210904599>
 }
 
 return allowedUsers
