d5547da930ea4a9ec066e997adff4be07f5a0858
scriptclient <rocksd333@gmail.com>
2025-10-08 21:36:59 -0300

    Key Manager: joaopauloPROplay (válida 15/10)

--- Users.lua
+++ Users.lua
@@ -71,7 +71,8 @@ allowedUsers = {
     ["boli256"] = true,   -- 72° boli256 -- BetaTeste -- <@1162443916688236657>,
     ["aslansayd8"] = true,   -- 73° aslansayd8 -- 11/10 -- <@1416226728518615040>,
     ["pedroalvim20"] = true,   -- 74° pedroalvim20 -- 11/10 -- <@1420501038624014346>,
-    ["matmat2345"] = true,   -- 75° matmat2345 -- 15/10 -- <@1399524275857129637>
+    ["matmat2345"] = true,   -- 75° matmat2345 -- 15/10 -- <@1399524275857129637>,
+    ["joaopauloPROplay"] = true,   -- 76° joaopauloPROplay -- 15/10 -- <@1223790591691718660>
 }
 
 return allowedUsers
