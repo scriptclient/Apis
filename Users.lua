64adecda3a7ff1205a672be5adc8134add979f14
scriptclient <rocksd333@gmail.com>
2025-10-09 02:01:04 -0300

    Key Manager: derrok0076 (válida permanente)

--- Users.lua
+++ Users.lua
@@ -67,7 +67,8 @@ allowedUsers = {
     ["citat5hu"] = true,   -- 79° citat5hu -- 11/10 -- <@1189833657473581169>,
     ["HBT_JU7cLA0MMP"] = true,   -- 80° HBT_JU7cLA0MMP -- 09/12 -- <@1292849634414428325>,
     ["Joaodopaograo"] = true,   -- 81° Joaodopaograo -- 15/10 -- <@1125619879060447252>,
-    ["acapsavass2"] = true,   -- 69° acapsavass2 -- 09/11 -- <@1166162590808936599>
+    ["acapsavass2"] = true,   -- 69° acapsavass2 -- 09/11 -- <@1166162590808936599>,
+    ["derrok0076"] = true,   -- 70° derrok0076 -- permanente -- <@1071176924094803989>
 }
 
 return allowedUsers
