7e1f0a7ceaa9cc2405d218c443a317b99d85f62a
scriptclient <rocksd333@gmail.com>
2025-10-09 01:39:31 -0300

    Key Removed: MG_esquadrao

--- Users.lua
+++ Users.lua
@@ -36,7 +36,6 @@ allowedUsers = {
     ["NikitaGavrileiko"] = true,   -- 35° NikitaGavrileiko -- 05/11 -- Sem
     ["Itz_Mariena"] = true,   -- 37° Itz_Mariena -- 05/12 -- Sem
     ["jhowzzzzzzzzzzz"] = true,   -- 40° jhowzzzzzzzzzzz -- 12/10 -- <@1376599474150838293>
-    ["MG_esquadrao"] = true,   -- 41° MG_esquadrao -- 09/10 -- <@1121893001397014590>
     ["brunoeduardo87"] = true,   -- 45° brunoeduardo87 -- 01/11 -- ymxxm
     ["x_P3drox"] = true,   -- 71° x_P3drox -- Permanente -- <@1424571061369966602>
     ["ponchatsg4"] = true,   -- 49° ponchatsg4 -- 13/10 -- <@1412938925139562537>
