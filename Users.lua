99dc8a4c8bc3c81af238bd95d33494f5f97497d8
scriptclient <rocksd333@gmail.com>
2025-10-09 23:53:34 -0300

    Key Manager: Zcode98 (válida 09/03)

--- Users.lua
+++ Users.lua
@@ -80,7 +80,8 @@ allowedUsers = {
     ["mocoto15737"] = true,   -- 79° mocoto15737 -- 16/10 -- <@1067950341162815488>,
     ["Lua099349"] = true,   -- 80° Lua099349 -- Permanente -- <@1411564076231823390>,
     ["cademeudinheiromeu_0"] = true,   -- 81° cademeudinheiromeu_0 -- 09/02 -- <@1418245676105728167>,
-    ["Gabrielzinx_S2"] = true,   -- 82° Gabrielzinx_S2 -- permanente -- <@1383242790133043260>
+    ["Gabrielzinx_S2"] = true,   -- 82° Gabrielzinx_S2 -- permanente -- <@1383242790133043260>,
+    ["Zcode98"] = true,   -- 83° Zcode98 -- 09/03 -- <@1288222278550687755>
 }
 
 return allowedUsers
