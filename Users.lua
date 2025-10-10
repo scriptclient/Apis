e48a9f2864539ddc006a7922425ccf6afa82b8c9
scriptclient <rocksd333@gmail.com>
2025-10-10 20:08:57 -0300

    Key Manager: tt (válida perm)

--- Users.lua
+++ Users.lua
@@ -91,7 +91,8 @@ allowedUsers = {
     ["Novo"] = true,   -- 91° Novo -- permanente -- <@1401226694324523113>,
     ["CaoHieu_32953"] = true,   -- 92° CaoHieu_32953 -- Permanente -- <@1414311802673692912>,
     ["icaro_scripter"] = true,   -- 94° icaro_scripter -- 10/11 -- <@1323323672437588030>,,
-    ["cauiuuuoooiii"] = true,   -- 94° cauiuuuoooiii -- 10/11 -- <@1323323672437588030>
+    ["cauiuuuoooiii"] = true,   -- 94° cauiuuuoooiii -- 10/11 -- <@1323323672437588030>,
+    ["tt"] = true,   -- 94° tt -- perm -- <@1132555470960349294>
 }
 
 return allowedUsers
