1cd9a325f06df9e202e896321b1761a4012ba05e
scriptclient <rocksd333@gmail.com>
2025-10-10 01:02:34 -0300

    Key Removed: sandegar2

--- Users.lua
+++ Users.lua
@@ -36,7 +36,6 @@ allowedUsers = {
     ["lufe_s"] = true,   -- 61° lufe_s -- 27/10 -- <@1354736279962652712>,
     ["firecrowblackice"] = true,   -- 63° firecrowblackice -- 07/03 -- <@1221327254425043036>,
     ["amigu_52"] = true,   -- 65° amigu_52 -- 17/10 -- <@1191124945594499133>,
-    ["sandegar2"] = true,   -- 68° sandegar2 -- 10/10 -- <@1425187663832678410>,
     ["MeninoDaRoca9"] = true,   -- 69° MeninoDaRoca9 -- 07/11 -- <@1407784397423251557>,
     ["higginsemily3"] = true,   -- 70° higginsemily3 -- perm -- <@1395310454259322942>,
     ["Kaua_452"] = true,   -- 72° Kaua_452 -- Modder -- <@1324502380225495077>,
