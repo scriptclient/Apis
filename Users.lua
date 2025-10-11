20b54072e10347422412c9453371f0570aac51fd
scriptclient <rocksd333@gmail.com>
2025-10-11 19:28:36 -0300

    Key Manager: Ruan3rdaccount2 (válida 14/10)

--- Users.lua
+++ Users.lua
@@ -98,7 +98,8 @@ allowedUsers = {
     ["scout7ixs"] = true,   -- 98° scout7ixs -- teste -- <@1401226694324523113>,
     ["amberoasis642"] = true,   -- 99° amberoasis642 -- 18/10 -- <@1411353791722160141>,    ["MIGUEL2000MU1"] = true,   -- 101° MIGUEL2000MU1 -- 11/3 -- <@1056245251628613632>,
     ["tubers93_ofc933"] = true,   -- 101° tubers93_ofc933 -- 11/11 -- <@1409994332500394065>,,
-    ["Gustavo_kez2"] = true,   -- 103° Gustavo_kez2 -- 18/10 -- <@1398313658169163857>
+    ["Gustavo_kez2"] = true,   -- 103° Gustavo_kez2 -- 18/10 -- <@1398313658169163857>,
+    ["Ruan3rdaccount2"] = true,   -- 103° Ruan3rdaccount2 -- 14/10 -- <@878591647707967498>
 }
 
 return allowedUsers
