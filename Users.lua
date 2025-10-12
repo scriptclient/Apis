50cce6238a0f148865d7662da4ea985756bb5ba6
scriptclient <rocksd333@gmail.com>
2025-10-12 17:35:45 -0300

    Key Manager: BOPOH_XXL (válida 15/10)

--- Users.lua
+++ Users.lua
@@ -113,7 +113,8 @@ allowedUsers = {
     ["Henriquea973"] = true,   -- 115° Henriquea973 -- 12/11 -- <@1285953278940807170>,
     ["RAFAELTX9"] = true,   -- 116° RAFAELTX9 -- 12/11 -- <@1313228292442034207>,
     ["hsuahj_62"] = true,   -- 116° hsuahj_62 -- 19/10 -- <@1394748106527997952>,
-    ["Galipexxzk"] = true,   -- 117° Galipexxzk -- 19/10 -- <@1403103958263795712>
+    ["Galipexxzk"] = true,   -- 117° Galipexxzk -- 19/10 -- <@1403103958263795712>,
+    ["BOPOH_XXL"] = true,   -- 118° BOPOH_XXL -- 15/10 -- <@1122324429754597386>
 }
 
 return allowedUsers
