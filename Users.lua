f0cea644f5dea6de8d8c4ffd1d3855255bf9550e
scriptclient <rocksd333@gmail.com>
2025-10-14 11:20:40 -0300

    Key Removed: Luangabry5

--- Users.lua
+++ Users.lua
@@ -118,8 +118,7 @@ allowedUsers = {
     ["Victhomxd"] = true,   -- 121° Victhomxd -- permanente -- <@1190835905116766210>,
     ["i"] = true,   -- 121° i -- dj -- ",
     ["eojaonevd41"] = true,   -- 122° eojaonevd41 -- permanente -- <@1349573143609741374>,
-    ["benanada2c"] = true,   -- 123° benanada2c -- permanente -- <@1136469950408708256>,
-    ["Luangabry5"] = true,   -- 124° Luangabry5 -- staff -- <@1100936474381254778>,
+    ["benanada2c"] = true,   -- 123° benanada2c -- permanente -- <@1136469950408708256>,,
     ["01Xxx09"] = true,   -- 124° 01Xxx09 -- 14/03 -- <@1363573061038903406>
 }
 
