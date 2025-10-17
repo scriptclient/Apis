68e9cf0d96850b54765f0497bdcd922627085260
scriptclient <rocksd333@gmail.com>
2025-10-17 06:56:00 -0300

    Key Manager: nobruzin0904 (válida 17/12)

--- Users.lua
+++ Users.lua
@@ -112,7 +112,8 @@ allowedUsers = {
     ["ERROR404_YS"] = true,   -- 116° ERROR404_YS -- 16/03 -- <@1413283999974953070>,
     ["doritos55876"] = true,   -- 117° doritos55876 -- 19/10 -- <@1317885329767665747>,
     ["fuguetinholiv"] = true,   -- 118° fuguetinholiv -- Permanente -- <@1426625512624750672>,
-    ["318789bhute"] = true,   -- 119° 318789bhute -- 20/10 -- <@1416542887251804284>
+    ["318789bhute"] = true,   -- 119° 318789bhute -- 20/10 -- <@1416542887251804284>,
+    ["nobruzin0904"] = true,   -- 117° nobruzin0904 -- 17/12 -- <@1106362755759493271>
 }
 
 return allowedUsers
