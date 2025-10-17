e3d54fac8e9777cf57579bdbdf7c7214397b6bd7
scriptclient <rocksd333@gmail.com>
2025-10-17 15:28:08 -0300

    Key Manager: ADMIN_ADUSE2456 (válida 24/10)

--- Users.lua
+++ Users.lua
@@ -114,7 +114,8 @@ allowedUsers = {
     ["fuguetinholiv"] = true,   -- 118° fuguetinholiv -- Permanente -- <@1426625512624750672>,
     ["318789bhute"] = true,   -- 119° 318789bhute -- 20/10 -- <@1416542887251804284>,
     ["nobruzin0904"] = true,   -- 117° nobruzin0904 -- 17/12 -- <@1106362755759493271>,
-    ["64y01"] = true,   -- 118° 64y01 -- 17/12 -- <@1326853664203341867>
+    ["64y01"] = true,   -- 118° 64y01 -- 17/12 -- <@1326853664203341867>,
+    ["ADMIN_ADUSE2456"] = true,   -- 119° ADMIN_ADUSE2456 -- 24/10 -- <@1428419697199681568>
 }
 
 return allowedUsers
