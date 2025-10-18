f2103b51b0da6ded324111497fa452eaddd025f8
scriptclient <rocksd333@gmail.com>
2025-10-17 22:20:06 -0300

    Key Manager: nvm mika_angel019 (válida 20/10)

--- Users.lua
+++ Users.lua
@@ -115,7 +115,8 @@ allowedUsers = {
     ["Jean123wrtp0w"] = true,   -- 120° Jean123wrtp0w -- 24/10 -- <@1414074166512062644>,
     ["witor3607"] = true,   -- 121° witor3607 -- 20/10 -- <@1396881223066063030>,,
     ["Infernal_King000"] = true,   -- 119° Infernal_King000 -- 17/11 -- <@1270931079364087818>,
-    ["kauanovo356"] = true,   -- 119° kauanovo356 -- 17/12 -- <@965751295711539281>
+    ["kauanovo356"] = true,   -- 119° kauanovo356 -- 17/12 -- <@965751295711539281>,
+    ["nvm mika_angel019"] = true,   -- 120° nvm mika_angel019 -- 20/10 -- <@1261069918078767174>
 }
 
 return allowedUsers
