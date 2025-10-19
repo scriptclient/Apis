9d530ab4bb4000159df3485a0c7f99e88ed85177
scriptclient <rocksd333@gmail.com>
2025-10-19 01:12:43 -0300

    Key Removed: rio_darkA

--- Users.lua
+++ Users.lua
@@ -116,7 +116,6 @@ allowedUsers = {
     ["CabeloBATATEIRU"] = true,   -- 117° CabeloBATATEIRU -- 21/10 -- <@1429129463567749242>,
     ["emma745419"] = true,   -- 118° emma745419 -- 18/11 -- <@760717484042092624>,
     ["jeff_yowa"] = true,   -- 117° jeff_yowa -- divulgador -- <@1397262082767982633>,
-    ["rio_darkA"] = true,   -- 118° rio_darkA -- 23/10 -- <@1320050504037826570>
 }
 
 return allowedUsers
