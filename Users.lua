4419499a417f0d2f8191d6f201e7869ac547e6d1
scriptclient <rocksd333@gmail.com>
2025-10-19 07:52:59 -0300

    Key Manager: MIGUELWWW22 (válida 19/12)

--- Users.lua
+++ Users.lua
@@ -116,7 +116,8 @@ allowedUsers = {
     ["CabeloBATATEIRU"] = true,   -- 117° CabeloBATATEIRU -- 21/10 -- <@1429129463567749242>,
     ["emma745419"] = true,   -- 118° emma745419 -- 18/11 -- <@760717484042092624>,
     ["jeff_yowa"] = true,   -- 117° jeff_yowa -- divulgador -- <@1397262082767982633>,
-    ["rio_darkA"] = true,   -- 118° rio_darkA -- 26/10 -- <@1320050504037826570>
+    ["rio_darkA"] = true,   -- 118° rio_darkA -- 26/10 -- <@1320050504037826570>,
+    ["MIGUELWWW22"] = true,   -- 119° MIGUELWWW22 -- 19/12 -- <@1223775248026042408>
 }
 
 return allowedUsers
