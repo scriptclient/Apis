94d0c0078009c0cda8c84a4759d64f51d8a645b1
scriptclient <rocksd333@gmail.com>
2025-10-19 20:21:03 -0300

    Key Manager: CUZCUZ12900 (válida 01/01)

--- Users.lua
+++ Users.lua
@@ -119,7 +119,8 @@ allowedUsers = {
     ["jsnsienhsienj"] = true,   -- 118° jsnsienhsienj -- permanente -- <@771039442957500487>,
     ["sigarrae"] = true,   -- 119° sigarrae -- 01/11 -- <@1362115295749341204>,,
     ["Engg7819"] = true,   -- 121° Engg7819 -- 19/11 -- <@1397109459041521674>,
-    ["Mariozinhozezo"] = true,   -- 122° Mariozinhozezo -- 1/11 -- <@1155356129971404850>
+    ["Mariozinhozezo"] = true,   -- 122° Mariozinhozezo -- 1/11 -- <@1155356129971404850>,
+    ["CUZCUZ12900"] = true,   -- 122° CUZCUZ12900 -- 01/01 -- <@1376637968768303104>
 }
 
 return allowedUsers
