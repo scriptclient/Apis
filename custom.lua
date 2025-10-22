2397942a775ad3299d45eb7d38b396a41c9d9d98
scriptclient <rocksd333@gmail.com>
2025-10-22 18:18:32 -0300

    Update custom.lua

--- custom.lua
+++ custom.lua
@@ -2,7 +2,4 @@
 return {
     ["pozella999"] = "P o z z",
     ["N0OB_BRAINR0T"] = "PVPZEIRO",
-    ["Lawliet_BR007"] = "zMedina",    
-    ["LULA13XVDEO"] = "Obscuro666",
-    ["dripcliento"] = "Rei Delas",
 }
