e07cd54bb83324492e63fd34f54d4e22a31e8a10
scriptclient <rocksd333@gmail.com>
2025-10-22 16:06:11 -0300

    Update custom.lua

--- custom.lua
+++ custom.lua
@@ -1,8 +1,8 @@
 ------/// NICKS - PERSON ///------
 return {
-    ["Kaua_452"] = "Supervisor",
     ["pozella999"] = "P o z z",
     ["N0OB_BRAINR0T"] = "PVPZEIRO",
-    ["Lawliet_BR007"] = "zMedina",    ["LULA13XVDEO"] = "Obscuro666",
+    ["Lawliet_BR007"] = "zMedina",    
+    ["LULA13XVDEO"] = "Obscuro666",
     ["dripcliento"] = "Rei Delas",
 }
