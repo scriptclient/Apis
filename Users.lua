2b1229547f9d72be759c711c5ddb3e441c1849cf
scriptclient <rocksd333@gmail.com>
2025-10-25 14:54:16 -0300

    Key Removed: makov_666

--- Users.lua
+++ Users.lua
@@ -71,7 +71,6 @@ allowedUsers = {
     ["MalkiewXH7722"] = true,   -- 70° MalkiewXH7722 -- 25/12 -- <@1424136578556498111>,
     ["1MIGUEL1BLACK1"] = true,   -- 71° 1MIGUEL1BLACK1 -- 30/10 -- <@991014244248076289>,
     ["lllllIllIllIllIllIlIlI"] = true,   -- 72° lllllIllIllIllIllIlIlI -- 28/10 -- <@1425297452805783725>,
-    ["makov_666"] = true,   -- 73° makov_666 -- 27/10 -- <@1360015997959930071>
 }
 
 return allowedUsers
