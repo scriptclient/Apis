a1a37d5bb1a7848985ae278a5201fcbc4c19f246
scriptclient <rocksd333@gmail.com>
2025-10-25 11:03:23 -0300

    Painel Name: scout7ixs -> InvokeServer

--- custom.lua
+++ custom.lua
@@ -3,4 +3,5 @@ return {
     ["pozella999"] = "P o z z",
     ["N0OB_BRAINR0T"] = "PVPZEIRO",
     ["derrok0076"] = "Moderador",
+    ["scout7ixs"] = "InvokeServer | salva: <@1268023655548321865> | Tempo: 25/10",
 }
