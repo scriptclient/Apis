5304df0ff957a3553a4cf1d334bd5602d1e5b5e5
scriptclient <rocksd333@gmail.com>
2025-10-31 14:14:46 -0300

    Key Manager: Victhomxd (válida permanente)

--- Users.lua
+++ Users.lua
@@ -55,8 +55,7 @@ allowedUsers = {
     ["c009kjg"] = true,   -- 78° c009kjg -- perm -- <@867948874248110120>,
     ["Lawliet_BR007"] = true,   -- 80° Lawliet_BR007 -- 16/03 -- <@1265342595329032252>,
     ["Ru12be4ns73"] = true,   -- 82° Ru12be4ns73 -- 13/02 -- <@1420227217459515444>,
-    ["sung_Shadoww"] = true,   -- 83° sung_Shadoww -- 13/11 -- <@969292147541028905>,
-    ["Victhomxd"] = true,   -- 85° Victhomxd -- permanente -- <@1190835905116766210>,
+    ["sung_Shadoww"] = true,   -- 83° sung_Shadoww -- 13/11 -- <@969292147541028905>,    ["Victhomxd"] = true,   -- 134° Victhomxd -- permanente -- <@1190835905116766210>
     ["eojaonevd41"] = true,   -- 86° eojaonevd41 -- permanente -- <@1349573143609741374>,
     ["benanada2c"] = true,   -- 87° benanada2c -- permanente -- <@1136469950408708256>,
     ["01Xxx09"] = true,   -- 88° 01Xxx09 -- 14/03 -- <@1363573061038903406>,
