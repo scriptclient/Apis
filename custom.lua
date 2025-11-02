a4bcc6893cf0f66f0fa24427673b437887cea01d
scriptclient <rocksd333@gmail.com>
2025-11-01 21:44:34 -0300

    Painel Name: KcsLaQLbOwB -> divulgador

--- custom.lua
+++ custom.lua
@@ -2,4 +2,5 @@
 return {
     ["pozella999"] = "P o z z",
     ["N0OB_BRAINR0T"] = "PVPZEIRO",
-    ["derrok0076"] = "Moderador",}
+    ["derrok0076"] = "Moderador",
+    ["KcsLaQLbOwB"] = "divulgador | salva: <@1410756703049220147> | Tempo: 0/0",}
