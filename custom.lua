bfac4ff4eb95e25c7925dd943b8865584a904cdd
scriptclient <rocksd333@gmail.com>
2025-11-01 21:53:29 -0300

    Removido do painel: KcsLaQLbOwB (por the.vt)

--- custom.lua
+++ custom.lua
@@ -2,5 +2,4 @@
 return {
     ["pozella999"] = "P o z z",
     ["N0OB_BRAINR0T"] = "PVPZEIRO",
-    ["derrok0076"] = "Moderador",
-    ["KcsLaQLbOwB"] = "divulgador | salva: <@1410756703049220147> | Tempo: 0/0",}
+    ["derrok0076"] = "Moderador",}
