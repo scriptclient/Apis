cea2c1849e6f89fe93c79eee6dd18252dbfbe975
scriptclient <rocksd333@gmail.com>
2025-11-05 11:24:33 -0300

    Key Manager: ckexee (válida 05/01)

--- Users.lua
+++ Users.lua
@@ -126,7 +126,8 @@ allowedUsers = {
     ["BRUH_20380"] = true,   -- 146° BRUH_20380 -- 11/11 -- <@1434726118476218520>,
     ["VitorX576"] = true,   -- 147° VitorX576 -- permanente -- <@1346919904149897381>,
     ["Hellointesljw"] = true,   -- 148° Hellointesljw -- 07/11 -- <@556616579526295579>,
-    ["Itzpret4"] = true,   -- 149° Itzpret4 -- 11/11 -- <@1391485303880941720>
+    ["Itzpret4"] = true,   -- 149° Itzpret4 -- 11/11 -- <@1391485303880941720>,
+    ["ckexee"] = true,   -- 130° ckexee -- 05/01 -- <@1288222278550687755>
 }
 
 return allowedUsers
