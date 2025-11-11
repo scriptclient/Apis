a44d86b87355c3441dad390c0b9d37e0eda589bf
scriptclient <rocksd333@gmail.com>
2025-11-11 17:35:05 -0300

    Key Manager: im_sattz7 (válida 16/11)

--- Users.lua
+++ Users.lua
@@ -127,7 +127,8 @@ allowedUsers = {
     ["Orrdaad"] = true,   -- 127° Orrdaad -- 11/12 -- <@1275565654434512950>,
     ["Ryehd52835"] = true,   -- 127° Ryehd52835 -- permanente -- <@1436705944607260712>,
     ["henriqubrasil"] = true,   -- 128° henriqubrasil -- 16/11 -- <@1382862012228436209>,
-    ["melsinha_1502"] = true,   -- 129° melsinha_1502 -- 27/11 -- <@1426275677174366229>
+    ["melsinha_1502"] = true,   -- 129° melsinha_1502 -- 27/11 -- <@1426275677174366229>,
+    ["im_sattz7"] = true,   -- 130° im_sattz7 -- 16/11 -- <@1369780252724690986>
 }
 
 return allowedUsers
