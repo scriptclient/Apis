03be184c9cccaaecfb989bbb7faf80461023cf11
scriptclient <rocksd333@gmail.com>
2025-11-15 18:12:20 -0300

    Key Manager: epilif2039 (válida 18/11)

--- Users.lua
+++ Users.lua
@@ -109,6 +109,7 @@ allowedUsers = {
     ["Sae_itoshi109932"] = true,   -- 109° Sae_itoshi109932 -- permanente -- <@1308206207068147763>,
     ["Rip_Samu2ss"] = true,   -- 110° Rip_Samu2ss -- Permanente -- <@1186461899144044544>,
     ["COMOVC_SUAMAE"] = true,   -- 111° COMOVC_SUAMAE -- 19/11 -- <@1414260263141642350>,
+    ["epilif2039"] = true,   -- 112° epilif2039 -- 18/11 -- <@1392459796975718411>
 }
 
 return allowedUsers
