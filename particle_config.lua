bce75b56de6152ac5e7ec8f61768b9388bda8b6e
scriptclient <rocksd333@gmail.com>
2026-04-11 11:34:17 -0300

    Create particle_config.lua

---
+++ particle_config.lua
@@ -0,0 +1,10 @@
+return {
+    MaxParticles     = 30,
+    SpawnRate        = 0.10,
+    ParticleSize     = { Min = 6, Max = 10 },
+    ParticleSpeed    = { Min = 15, Max = 25 },
+    ParticleLifetime = 6.0,
+    GlowIntensity    = 0.3,
+    WindDrift        = 0,
+    Gravity          = 0,
+}
